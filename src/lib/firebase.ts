2b7f1d24721d5b2f3c0f76301a99711b04fb1c48
Aditya Sapru <sapruads@gmail.com>
2025-10-24 21:49:44 +0000

    login failed again. is something wrong

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,16 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "your-api-key",
   authDomain: "posturetracker-6c60d.firebaseapp.com",
   projectId: "posturetracker-6c60d",
   storageBucket: "posturetracker-6c60d.appspot.com",
-  messagingSenderId: "935742398579",
-  appId: "1:935742398579:web:96e270273f53835c242e88",
+  messagingSenderId: "367039542911",
+  appId: "1:367039542911:web:53a69344234346e43f063a",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-
-if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && !(auth as any)._emulator) {
-    // connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-}
 
 export { app, auth };
